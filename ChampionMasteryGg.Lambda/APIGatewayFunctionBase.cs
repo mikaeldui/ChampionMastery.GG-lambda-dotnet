f05e963d031f58874225ca5ab7634c2479e2e79e
Mikael Dúi Bolinder <github@bolinder.uk>
2022-02-05 03:56:13 +0100

    Cleanup

--- ChampionMasteryGg.Lambda/APIGatewayFunctionBase.cs
+++ ChampionMasteryGg.Lambda/APIGatewayFunctionBase.cs
@@ -1,10 +1,4 @@
 ﻿using Amazon.Lambda.APIGatewayEvents;
-using Amazon.Lambda.Core;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Amazon.Lambda
 {
--- ChampionMasteryGg.Lambda/Function.cs
+++ ChampionMasteryGg.Lambda/Function.cs
@@ -1,12 +1,6 @@
-using Amazon.Lambda;
-using Amazon.Lambda.Core;
-using Amazon.Lambda.Model;
 using Amazon.Lambda.APIGatewayEvents;
-using Amazon.XRay.Recorder.Handlers.AwsSdk;
-using System.Threading.Tasks;
-using System.Text.Json;
 
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))] // Is this needed?
 
 namespace ChampionMasteryGg
 {
@@ -26,18 +20,12 @@ namespace ChampionMasteryGg
         {
             try
             {
-                if (invocationEvent.RawPath == "/")
-                {
-                    return Found(await cmggClient.GetHighscoresAsync());
-                }
-                else if (invocationEvent.RawPath == "/level")
-                {
-                    return Found(await cmggClient.GetTotalLevelHighscoresAsync());
-                }
-                else if (invocationEvent.RawPath == "/points")
-                {
-                    return Found(await cmggClient.GetTotalPointsHighscoresAsync());
-                }
+                if (invocationEvent.RawPath == "/")                
+                    return Found(await cmggClient.GetHighscoresAsync());                
+                else if (invocationEvent.RawPath == "/level")                
+                    return Found(await cmggClient.GetTotalLevelHighscoresAsync());                
+                else if (invocationEvent.RawPath == "/points")                
+                    return Found(await cmggClient.GetTotalPointsHighscoresAsync());                
                 else if (invocationEvent.RawPath.StartsWith("/champions"))
                 {
                     var championId = int.Parse(invocationEvent.RawPath["/champions/".Length..]);
